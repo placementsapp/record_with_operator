ad1e40ad2548466252ecc7d4751d1cfaf9913b53
Zubin Wang <zubin.wang@gmail.com>
2023-10-31 16:08:12 -0700

    add with_rwo_operator

--- lib/record_with_operator/operator.rb
+++ lib/record_with_operator/operator.rb
@@ -7,5 +7,13 @@ module RecordWithOperator
     def rwo_operator
       RequestStore.store[:rwo_operator]
     end
+
+    def with_rwo_operator(o)
+      original = rwo_operator
+      self.rwo_operator = o
+      yield
+    ensure
+      self.rwo_operator = original
+    end
   end
 end
